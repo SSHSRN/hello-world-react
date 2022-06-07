813cb7ea73987663fc9f063a527430e12291ae9e
SRIHARI <sshsrn@gmail.com>
2022-06-07 22:26:04 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 // import Greet from './components/Greet'; => for default export
-import {Greet} from './components/Greet'; // => for named export
+
+// import {Greet} from './components/Greet'; // => for named export
+
+import Greet from './components/Greet';
 
 import Welcome from './components/Welcome';
 
@@ -9,8 +12,22 @@ import Hello from './components/Hello';
 function App() {
   return (
     <div className="App">
-      <Greet/>
-      <Welcome/>
+      {/* <Greet/> */}
+
+      {/* Using props */}
+      <Greet name="Srihari" heroName="SSHSRN"/>
+      <Greet name="Tony Stark" heroName="IRON MAN"/>
+      <Greet name="Peter Parker" heroName="SPIDER MAN"/>
+      <Greet name="Eddie Brock" heroName="VENOM">
+        <p>Venom is a black symbiote.</p>
+      </Greet>
+      {/* <Welcome/> */}
+
+      {/* Using props for a classful component */}
+      <Welcome name="Srihari" heroName="SSHSRN"/>
+      <Welcome name="Tony Stark" heroName="IRON MAN"/>
+      <Welcome name="Peter Parker" heroName="SPIDER MAN"/>
+      <Welcome name="Eddie Brock" heroName="VENOM"/>
       <Hello/>
     </div>
   );
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -9,9 +9,27 @@ import React from 'react'
 // }
 
 // Es6 Arrow function representation:
-export const Greet = () => <h1>Hey SSHSRN!</h1>;
+// export const Greet = () => <h1>Hey SSHSRN!</h1>;
+
+// Using props
+const Greet = (props) => {
+    console.log(props);
+    // Printing the props will give an object with the properties given for the component in the place where it's called.
+    // Props is just an object containing the attributes and their values which have been passed
+    // return <h1>Hello {props.name} a.k.a {props.heroName}</h1>
+
+    return (
+        <div>
+            <h1>Hello {props.name} a.k.a {props.heroName}</h1>
+            {props.children}
+        </div>
+    )
+}
+
 
 // export default Greet
 // In default export, the name of the component can be changed in the file where it is imported.
 
 // The same is not possible in named export.
+
+export default Greet
